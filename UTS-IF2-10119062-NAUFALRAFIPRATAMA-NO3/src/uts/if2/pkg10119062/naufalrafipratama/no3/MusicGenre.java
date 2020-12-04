ebc4f159387c9a9d5672735a1f714f9414664705
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-12-04 22:03:20 +0700

    Add files via upload

---
+++ UTS-IF2-10119062-NAUFALRAFIPRATAMA-NO3/src/uts/if2/pkg10119062/naufalrafipratama/no3/MusicGenre.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119062.naufalrafipratama.no3;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : program ini berisi program untuk menampilkan
+ * genre musik
+ */
+public abstract class MusicGenre {
+    private String artistName;
+    
+    public String artisName() {
+        return artistName;
+    }
+    
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+    
+}
---
+++ UTS-IF2-10119062-NAUFALRAFIPRATAMA-NO3/src/uts/if2/pkg10119062/naufalrafipratama/no3/UTSIF210119062NAUFALRAFIPRATAMANO3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119062.naufalrafipratama.no3;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : program ini berisi program untuk menampilkan
+ * genre musik
+ */
+public class UTSIF210119062NAUFALRAFIPRATAMANO3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Country objCountry = new Country();
+        
+    }
+    
+}
